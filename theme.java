ce08c877530731002b12c6bee33a46d868033bce
vladislaaf <stanolevich.vlad@mail.ru>
2022-06-20 11:26:30 +0300

    delete

--- theme.java
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector(".btn-toggle");
-const theme = document.querySelector("#theme-link");
-btn.addEventListener("click", function() {
-        if (theme.getAttribute("href") == "styles/light-theme.css") {
-            theme.href = "styles/dark-theme.css";
-        } else {
-            theme.href = "styles/light-theme.css";
-        }
-    });
